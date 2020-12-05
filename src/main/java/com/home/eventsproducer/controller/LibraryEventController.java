dfd838d3a51b1bf956f0185e7c1460b3d8dfea61
Ankur1710 <ankurghosh.1710@outlook.com>
2020-12-05 00:17:19 -0500

    asynchronous post Behaviour two different Threads

--- src/main/java/com/home/eventsproducer/controller/LibraryEventController.java
+++ src/main/java/com/home/eventsproducer/controller/LibraryEventController.java
@@ -2,6 +2,7 @@ package com.home.eventsproducer.controller;
 
 import com.home.eventsproducer.domain.LibraryEvent;
 import com.home.eventsproducer.producer.LibEventProducer;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class LibraryEventController {
 
         @Autowired
@@ -18,7 +20,10 @@ public class LibraryEventController {
         @PostMapping("v1/libraryevent")
         public ResponseEntity<LibraryEvent> postLibraryEvent(@RequestBody LibraryEvent libraryEvent){
             //Invoke the Kafka Producer
+            log.info("before send library events");
             libEventProducer.sendLibraryEvent(libraryEvent);
+            log.info("after send library events");
+
             return ResponseEntity.status(HttpStatus.CREATED).body(libraryEvent);
         }
 
--- src/main/java/com/home/eventsproducer/producer/LibEventProducer.java
+++ src/main/java/com/home/eventsproducer/producer/LibEventProducer.java
@@ -7,7 +7,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.ListenableFutureCallback;
 
 @Service
 @Slf4j
@@ -24,13 +27,40 @@ public class LibEventProducer {
 
     public void sendLibraryEvent(LibraryEvent libraryEvent){
         try {
-            String payload = objectMapper.writeValueAsString(libraryEvent);
-            kafkaTemplate.send(topic,payload);
+            Integer key  = libraryEvent.getLibraryEventId();
+            String value = objectMapper.writeValueAsString(libraryEvent);
+           ListenableFuture<SendResult<Integer,String>> listenableFuture =
+                   kafkaTemplate.send(topic,key,value);
+           listenableFuture.addCallback(new ListenableFutureCallback<SendResult<Integer, String>>() {
+               @Override
+               public void onFailure(Throwable throwable) {
+                   handleFailure(key,value,throwable);
+               }
+
+               @Override
+               public void onSuccess(SendResult<Integer, String> result) {
+                handleSuccess(key,value,result);
+               }
+           });
         } catch (JsonProcessingException e) {
             e.printStackTrace();
         }
 
 
+    }
+
+    private void handleFailure(Integer key, String value, Throwable ex) {
+        log.error("error sending the message and the exception is: {} "+ex.getMessage());
+        try{
+            throw ex;
+        } catch (Throwable throwable) {
+           log.error("on failure: {} "+ throwable.getMessage());
+        }
+    }
+
+    private void handleSuccess(Integer key, String value, SendResult<Integer, String> result) {
+        log.info("message sent successfully for the key: {} and the value : {} , partition: {}"
+                + key+", "+value+", "+result.getRecordMetadata().partition());
     }
 
 
