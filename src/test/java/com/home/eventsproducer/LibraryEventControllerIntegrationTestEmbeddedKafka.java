cdbb1878ee550e53244beee2a1c6187188eec0d3
Ankur1710 <ankurghosh.1710@outlook.com>
2020-12-05 11:14:57 -0500

    integration test with embedded kafka

--- src/test/java/com/home/eventsproducer/LibraryEventControllerIntegrationTestEmbeddedKafka.java
+++ src/test/java/com/home/eventsproducer/LibraryEventControllerIntegrationTestEmbeddedKafka.java
@@ -3,16 +3,20 @@ package com.home.eventsproducer;
 import com.home.eventsproducer.domain.Book;
 import com.home.eventsproducer.domain.LibraryEvent;
 import com.home.eventsproducer.domain.LibraryEventType;
-
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.*;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.test.context.TestPropertySource;
 
-@SpringBootTest(classes = LibraryEventControllerIntegrationTest.class)
-public class LibraryEventControllerIntegrationTest {
+@SpringBootTest(classes = LibraryEventControllerIntegrationTestEmbeddedKafka.class,
+		webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@EmbeddedKafka(topics = {"library-events"},partitions = 3)
+@TestPropertySource(properties = {"spring.kafka.producer.bootstrap-servers=${spring.embedded.kafka.brokers}"})
+public class LibraryEventControllerIntegrationTestEmbeddedKafka {
 
 	@Autowired
 	TestRestTemplate testRestTemplate;
